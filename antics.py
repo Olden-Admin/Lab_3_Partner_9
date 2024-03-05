b1f82654064753c047a02fd2e4e17e2166eb9e12
ang7 <awu8300@gmail.com>
2024-03-05 14:17:42 -0800

    Add files via upload

--- antics.py
+++ antics.py
@@ -1,11 +1,36 @@
-def is_pangram(sentence):
-   """Return True if sentence is a pangram, False otherwise."""
-   alphabet = 'abcdefghijklmnopqrstuvwxyz'
-   for char in alphabet:
-      if char not in sentence.lower():
-         return False
-   return True
+# CIS-117 Lab3
+# description_____
+# Group 9
+# names: Angela Wu, ___, ___
 
-def is_palindrome(word):
-   """Return True if word is a palindrome, False otherwise."""
-   return word == word[::-1]
+print('hello testing')
+
+# test if input is abecedarian meaning in alphabetical order
+def is_abecedarian(word_in):
+    word = word_in.lower()
+    previous = word[0]
+    for i in word:
+        if i < previous:
+            return False
+        previous = i
+    return True
+
+# test is input has exactly two of same letters in string
+def is_dobloon(word):
+    for i in word:
+        count = 1
+        for j in word[1:]:
+            if i == j:
+                count += 1
+                if count > 2:
+                    return False
+        if count == 1:
+            return False
+        return True 
+
+
+
+print(is_abecedarian('bacd'))
+print(is_abecedarian('abcd'))
+print('if two:', is_dobloon('mammal'))
+print('if two:' , is_dobloon('noon')) 
