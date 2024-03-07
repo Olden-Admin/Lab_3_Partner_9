392ece471738a3a4d7af43e2808b7850666dff5f
newRepository <oldenk1@my.smccd.edu>
2024-03-07 01:46:16 -0500

    This corrects pangrram

--- antics.py
+++ antics.py
@@ -38,12 +38,14 @@ def isogram(word):
     return True
 
 def pangram(sentence):
-   """Return True if sentence is a pangram, False otherwise."""
-   alphabet = 'abcdefghijklmnopqrstuvwxyz'
-   for char in alphabet:
-       if char not in sentence.lower():
-           return False
-              
+    """Return True if sentence is a pangram, False otherwise."""
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    sentence = sentence.lower()
+    for char in alphabet:
+        if char not in sentence:
+            return False
+    return True
+
 def palindrome(word): 
    """Return True if word is a palindrome, False otherwise."""
    return word == word[::-1]
